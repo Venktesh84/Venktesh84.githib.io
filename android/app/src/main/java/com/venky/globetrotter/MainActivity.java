8860e93b7e07a8d3dc96ba5b1b26388ad0758bd2
venktesh84 <venktesh.007@rediffmail.com>
2021-08-21 16:17:19 +0100

    Android Config and Dep added

---
+++ android/app/src/main/java/com/venky/globetrotter/MainActivity.java
@@ -0,0 +1,5 @@
+package com.venky.globetrotter;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.venky.globetrotter',
+  appName: 'globetrotter',
+  webDir: 'dist/globetrotter',
+  bundledWebRuntime: false
+};
+
+export default config;
